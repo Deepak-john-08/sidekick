da52beaa4b9845aa619917c389c1e826828425f0
Deepak V <79626097+Deepak-john-08@users.noreply.github.com>
2026-09-06 21:55:04 +0530

    Change server port from 7860 to 10000

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ with gr.Blocks(title="Sidekick") as ui:
 if __name__ == "__main__":
     ui.launch(
         server_name="0.0.0.0",
-        server_port=int(os.environ.get("PORT", 7860)),
+        server_port=int(os.environ.get("PORT", 10000)),
         inbrowser=False,
         **LAUNCH_STYLE
     )
